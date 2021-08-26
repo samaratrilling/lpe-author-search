f7a6dfd80720f85be71fb803cec1768675d469d3
Samara Trilling <samara.trilling@gmail.com>
2021-08-26 12:00:57 -0400

    Add keywords, shell script and url list

---
+++ lpe-search.sh
@@ -0,0 +1,7 @@
+keywords="$(<./keywords.txt)"
+while IFS= read -r url; do
+    curl -L -s "$url" | grep -ioF "$keywords" |
+      while IFS= read -r keyword; do
+        echo "$url: $keyword"
+      done
+done < ./url_list.txt
